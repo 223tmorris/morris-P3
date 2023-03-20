5b2a531b4b8909dc1b14043624cd3e87d9c3e447
223tmorris <223tmorris@frhsd.com>
2023-03-20 10:08:38 -0400

    Completed to step 4 - added initial vue setup

--- site.js
+++ site.js
@@ -33,7 +33,9 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Tyler's Top 8 Movies",
+            owner: "Tyler",
+            github: "http://www.github.com/223tmorris/morris-P3"
          
       }
     },
